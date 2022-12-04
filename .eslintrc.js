7ac59f85d568a555b58825a1f624997b3ec64fd8
Jeremy Neander <jeremy.neander@gmail.com>
2022-12-04 12:54:05 -0600

    chore: import eslint and babel configs from dev-lint library

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,73 @@
-const {configureEslint} = require('@jneander/dev-lint')
+/*
+ * For reference:
+ * https://eslint.org/docs/user-guide/configuring
+ */
 
-module.exports = configureEslint({
-  browser: true,
-})
-
-module.exports.overrides.push({
+module.exports = {
   env: {
-    node: true,
+    browser: true,
+    es6: true,
+    mocha: true,
   },
 
-  files: ['./config/**/*.js', './scripts/**/*.js'],
-})
+  extends: [
+    'eslint:recommended',
+    'prettier',
+    'plugin:eslint-comments/recommended',
+    'plugin:promise/recommended',
+  ],
+
+  globals: {},
+
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+
+      files: ['./.eslintrc.js'],
+    },
+
+    {
+      files: ['./**/*.spec.js', './**/_specs_/**/*.js'],
+
+      globals: {
+        expect: 'writable',
+      },
+    },
+
+    {
+      env: {
+        node: true,
+      },
+
+      files: ['./config/**/*.js', './scripts/**/*.js'],
+    },
+  ],
+
+  parser: '@babel/eslint-parser',
+
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: false,
+    },
+
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+
+  plugins: ['import', 'prettier', 'promise', 'mocha'],
+  root: true,
+
+  rules: {
+    'eslint-comments/no-unused-disable': 'error',
+    'import/extensions': ['error', 'ignorePackages', {js: 'never'}],
+    'import/no-extraneous-dependencies': ['error', {devDependencies: true}],
+    'no-unused-vars': ['error', {argsIgnorePattern: '^_'}],
+    'prettier/prettier': 'error',
+    'arrow-body-style': 'off',
+    'prefer-arrow-callback': 'off',
+  },
+
+  settings: {},
+}
---
+++ babel.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  presets: [
+    [
+      '@babel/preset-env',
+
+      {
+        modules: false,
+
+        targets: {
+          browsers: [
+            'last 2 chrome versions',
+            'last 2 firefox versions',
+            'last 2 edge versions',
+            'last 2 ios versions',
+            'last 2 opera versions',
+            'last 2 safari versions',
+            'last 2 ChromeAndroid versions',
+          ],
+        },
+
+        useBuiltIns: false,
+      },
+    ],
+  ],
+}
